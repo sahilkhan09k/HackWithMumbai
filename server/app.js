824cf10f1fdc0d36d2bff2cff5151a59d8caa520
Gaurav Salunke <gauravsalunke252@gmail.com>
2026-02-08 11:03:08 +0530

    Update app.js configuration

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 const app = express();
 
 app.use(cors({
-    origin: [process.env.CORS_ORIGIN, 'http://localhost:5173', 'http://localhost:5174'],
+    origin: [process.env.CORS_ORIGIN, 'https://hack-with-mumbai.vercel.app'],
     credentials: true
 }))
 
