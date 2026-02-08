cdcfd205d00ecb58c116be24291e34fbaec997d9
Gaurav Salunke <gauravsalunke252@gmail.com>
2026-02-08 12:05:21 +0530

    Improve auth middleware error handling

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -5,19 +5,19 @@ import jwt from "jsonwebtoken"
 
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if (!token) {
+        throw new apiError(401, "Unauthorized request")
+    }
+
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-
-        if (!token) {
-            throw new apiError(401, "Unauthorized request")
-        }
-
         const decodedInfo = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
         const user = await User.findById(decodedInfo?._id).select("-password -refreshToken")
 
         if (!user) {
-            throw new apiError(401, "Invalid acces token provided")
+            throw new apiError(401, "Invalid access token provided")
         }
 
         // Check if user is banned
@@ -28,6 +28,9 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
         req.user = user
         next()
     } catch (error) {
-        throw new apiError(401, error?.message || "invalid access token")
+        if (error instanceof apiError) {
+            throw error;
+        }
+        throw new apiError(401, "Invalid or expired access token")
     }
 })
